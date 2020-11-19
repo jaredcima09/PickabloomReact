162da6984edbf61c4a172447022461e12df42c9d
jaredcima09 <jaredcimafranca@gmail.com>
2020-11-19 23:22:12 +0800

    initial commit

--- src/Card.js
+++ src/Card.js
@@ -3,10 +3,10 @@ import './Cards.css'
 const Card = ({ id, name, email, price }) => {
 
     return (
-        <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5 container'>
+        <div className='tc bg-light-pink dib br3 pa3 ma2 grow bw2 shadow-5 container'>
             {/* <img alt = 'photo' src={`https://robohash.org/${id}?200x200`} /> */}
             <img alt='photo' src={email[0]} style={{ height: '200px', width: '200px' }} />
-            <div>
+            <div className="cardContainer">
                 <h2>{name}</h2>
                 <p>{price}</p>
             </div>
